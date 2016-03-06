de77ef4e0cf73989d768e0de6da6680c023d4e52
Harry Chou <harrychou@gmail.com>
2016-03-06 18:34:30 -0500

    app.js with template

---
+++ app.js
@@ -0,0 +1,23 @@
+/*jslint devel:true, node: true */
+'use strict';
+var express = require('express');
+var app = express();
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.get('/books', function (req, res) {
+    res.send('Hello Books');
+});
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log('Error:', err);
+    }
+    console.log('running server on port ' + port);
+});
