f881e8dc846fee22c9004cb906abfc5ec20abe01
Harry Chou <harrychou@gmail.com>
2016-03-11 18:20:38 -0500

    see

--- app.js
+++ app.js
@@ -2,17 +2,37 @@
 'use strict';
 var express = require('express');
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 
+// serve everything in /public folder as static content
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 
+// configure view tempalte engine
+app.set('views', './src/views');
+
+// jade config
+//app.set('view engine', 'jade');
+
+// handlebars
+//var handlebars = require('express-handlebars');
+//app.engine('.hbs', handlebars({extrem: '.hbs'}));
+//app.set('view engine', '.hbs');
+
+// ejs
+app.set('view engine', 'ejs');
+
+var nav = [
+  {link: '/books', text: 'Books'},
+  {link: '/authors', text: 'Authros'}
+];
+
+// book route
+var booksRouter = require('./src/routes/bookRoute')(nav);
+app.use('/books', booksRouter);
+
+// route '/'  to index
 app.get('/', function (req, res) {
-  res.send('Hello World');
-});
-
-app.get('/books', function (req, res) {
-  res.send('Hello Books');
+  res.render('index', {title: 'title goes here', nav: nav});
 });
 
 app.listen(port, function (err) {
--- gulpfile.js
+++ gulpfile.js
@@ -3,55 +3,48 @@ var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var nodemon = require('gulp-nodemon');
 
-var jsFiles = ['*.js', 'src/**/*.js'];
+// all js files in the root directory, and in src folder
+var jsFiles = ['*.js', 'src/**/*.js', 'public/js/*.js'];
 
-gulp.task('style', function () {
-    return gulp.src(jsFiles)
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {
-            verbose: true
-        }))
-        .pipe(jscs());
+// use jshint and jscs to check syntax on jsFiles
+gulp.task('style', function() {
+  return gulp.src(jsFiles)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
+    .pipe(jscs())
+    .pipe(jscs.reporter());
 });
 
-gulp.task('inject', function () {
-    var wiredep = require('wiredep').stream;
-    var inject = require('gulp-inject');
+gulp.task('inject', function() {
+  var wiredep = require('wiredep').stream;
+  var wireDepOptions = {
+    bowerJson: require('./bower.json'),
+    directory: './public/lib',
+    ignorePath: '../../public'
+  };
 
-    var injectSrc = gulp.src(['./public/css/*.css',
-                              './public/js/*.js'], {
-        read: false
+  var inject = require('gulp-inject');
+  var injectSource = gulp.src(['./public/css/*.css', './public/js/*.js']);
+  var injectOptions = {ignorePath: '/public'};
+
+  return gulp.src('./src/views/*.ejs')
+    .pipe(wiredep(wireDepOptions))
+    .pipe(inject(injectSource, injectOptions))
+    .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function() {
+  var options = {
+    script: 'app.js',
+    delayTime: 1,
+    env: {
+      'PORT': 7000
+    },
+    watch: jsFiles
+  };
+
+  return nodemon(options)
+    .on('restart', function(en) {
+      console.log(' .... restarting .... ');
     });
-
-    var injectOptions = {
-        ignorePath: '/public'
-    };
-
-    var options = {
-        bowerJson: require('./bower.json'),
-        directory: './public/lib',
-        ignorePath: '../../public'
-    }
-
-    return gulp.src('./src/views/*.html')
-        .pipe(wiredep(options))
-        .pipe(inject(injectSrc, injectOptions))
-        .pipe(gulp.dest('./src/views'));
-
-})
-
-gulp.task('serve', ['style', 'inject'], function () {
-    var options = {
-        script: 'app.js',
-        delayTime: 1,
-        env: {
-            'PORT': 3000
-        },
-        watch: jsFiles
-    }
-
-    return nodemon(options)
-        .on('restart', function (ev) {
-            console.log('Restarting....');
-        })
-})
+});
